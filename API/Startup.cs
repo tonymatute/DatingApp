f96a5820bf01c27e0117ca6c5b9759bc68e36bbf
TonyMatute <tony.matute@comcast.net>
2020-10-29 12:02:43 -0500

    End of Session 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,9 +1,9 @@
 using API.Extentions;
+using API.MiddleWare;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -28,10 +28,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
