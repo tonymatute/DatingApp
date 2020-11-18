e16cf5e8e5e899d3d898dba10c32de7834881896
TonyMatute <tony.matute@comcast.net>
2020-11-18 15:18:31 -0600

    Section 19 - pre switch db to postgress

---
+++ API/Controllers/FallbackController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace API.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), 
+                "wwwroot", "index.html"), "text/HTML");
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,55 +1,59 @@
 using API.Extentions;
 using API.MiddleWare;
 using API.SignalR;
+using EmailService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace API
-{
-    public class Startup
-    {
+namespace API {
+    public class Startup {
         private readonly IConfiguration _config;
 
-        public Startup(IConfiguration config)
-        {
+        public Startup (IConfiguration config) {
             _config = config;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddApplicationServices(_config);
-            services.AddControllers();
-            services.AddCors();
-            services.AddIdentityServices(_config);
-            services.AddSignalR();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddApplicationServices (_config);
+            services.AddControllers ();
+            services.AddCors ();
+            services.AddIdentityServices (_config);
+            services.AddSignalR ();
+            var emailConfig = _config
+                .GetSection ("EmailConfiguration")
+                .Get<EmailConfiguration> ();
+            services.AddSingleton (emailConfig);
+            services.AddScoped<IEmailSender, EmailSender> ();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<ExceptionMiddleware>();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseMiddleware<ExceptionMiddleware> ();
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseCors(policy => policy.AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials()
-                .WithOrigins("https://localhost:4200"));
+            app.UseCors (policy => policy.AllowAnyHeader ()
+                .AllowAnyMethod ()
+                .AllowCredentials ()
+                .WithOrigins ("https://localhost:4200"));
 
-            app.UseAuthentication();
+            app.UseAuthentication ();
+            app.UseAuthorization ();
 
-            app.UseAuthorization();
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapHub<PresenceHub>("hubs/presence");
-                endpoints.MapHub<MessageHub>("hubs/message");
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+                endpoints.MapHub<PresenceHub> ("hubs/presence");
+                endpoints.MapHub<MessageHub> ("hubs/message");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
