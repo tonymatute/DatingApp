181f58cfc2978ab537111f608b3ab1ec3b21b943
TonyMatute <tony.matute@comcast.net>
2020-11-19 08:59:03 -0600

    Photo Management Challenge

--- client/src/app/_models/photo.ts
+++ client/src/app/_models/photo.ts
@@ -1,5 +1,7 @@
 export interface Photo {
-    id: number;
-    url: string;
-    isMain: boolean;
-  }
+  id: number;
+  url: string;
+  isMain: boolean;
+  isApproved: boolean;
+  username?: string;
+}
--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Photo } from '../_models/photo';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -20,4 +21,15 @@ export class AdminService {
     return this.http.post(this.baseUrl + 'admin/edit-roles/' + username + '?roles=' + roles, {});
   }
 
+  getPhotoForApproval() {
+    return this.http.get<Photo[]>(this.baseUrl + "admin/photos-to-moderate");    
+  }
+
+  approvePhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/approve-photo/' + photoId, {});
+  }
+
+  rejectPhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/reject-photo/' + photoId,{});
+  }
 }
